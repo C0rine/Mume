6a64db4504c38d9daf76531b6a8dfb033ce3ff49
C0rine <Corine_J@MSN.com>
2016-01-08 14:46:20 +0100

    made rough setup of edit activity

--- Mume/app/src/main/java/nl/mprog/mume/EditActivity.java
+++ Mume/app/src/main/java/nl/mprog/mume/EditActivity.java
@@ -1,7 +1,13 @@
 package nl.mprog.mume;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class EditActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+    }
+
+    // Inflate menu facilitate help-button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        return true;
+    }
+
+
+    // Implementation of action-bar / menu functionalities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // Open Help
+        if (id == R.id.searchhelp_menubutton) {
+            Toast.makeText(this, "Help!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
