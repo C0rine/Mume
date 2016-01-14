2edf2d0c97b1b70bc521690c6545226d7c041bc3
C0rine <Corine_J@MSN.com>
2016-01-14 14:47:07 +0100

    improved the UI for displaying the search results

--- Mume/app/src/main/java/nl/mprog/mume/Adapters/ResultsAdapter.java
+++ Mume/app/src/main/java/nl/mprog/mume/Adapters/ResultsAdapter.java
@@ -8,6 +8,7 @@
 package nl.mprog.mume.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+
+import java.util.ArrayList;
 
 import nl.mprog.mume.Classes.ImageRetriever;
 import nl.mprog.mume.Classes.QueryMaker;
@@ -31,10 +36,20 @@ public class ResultsAdapter extends BaseAdapter {
     private String[] objectids;
     private ImageRetriever imageRetriever;
 
+    private VolleySingleton volleySingleton;
+    private ImageLoader imageLoader;
+    final ArrayList<Bitmap> bitmapArrayList;
+
     public ResultsAdapter(Context c, String[] artistnames, String[] objectids) {
+
+        // constructor
         this.context = c;
         this.artistnames = artistnames;
         this.objectids = objectids;
+
+        volleySingleton = VolleySingleton.getInstance();
+        imageLoader = volleySingleton.getmImageLoader();
+        bitmapArrayList = new ArrayList<Bitmap>();
 
         imageRetriever = new ImageRetriever();
     }
@@ -66,12 +81,12 @@ public class ResultsAdapter extends BaseAdapter {
         // 1) a textview to hold the artistname
         final TextView theTextview = (TextView) convertView.findViewById(R.id.thumbnailtitle_textview);
         String textviewtext = artistnames[position];
-        //theTextview.setText(textviewtext);
+        theTextview.setText(textviewtext);
 
         // 2) an imageview to hold the thumbnail
         final ImageView theImageView = (ImageView) convertView.findViewById(R.id.thumbnail_imageview);
-        theImageView.setImageResource(R.mipmap.art_nightwatch_square);
-        theImageView.setAdjustViewBounds(true);
+        theImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        theImageView.setImageResource(R.mipmap.image_icon);
 
         // 3) retrieve the thumbnail imageRetriever url for the imageview
         // create the request queue using Volley
@@ -81,12 +96,34 @@ public class ResultsAdapter extends BaseAdapter {
         QueryMaker queryMaker = new QueryMaker();
         queryMaker.setSearchtype("image");
 
-        // For each objectid retrieve an image url for the thumbnail
         // resource: http://stackoverflow.com/questions/28120029
         imageRetriever.retrieveURL(requestQueue, queryMaker, objectids[position], new ImageRetriever.VolleyCallback() {
             @Override
             public void onSuccess(String result) {
-                theTextview.setText(result);
+                //theTextview.setText(result);
+                String URL = result;
+                if (URL != null) {
+
+                    // use the image loader to get the image
+                    imageLoader.get(URL, new ImageLoader.ImageListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Failure: set a standard image if this image could not load
+                            theImageView.setImageResource(R.mipmap.image_notavailable_icon);
+                            Log.e("RESULTSADAPTER", "There was an error in loading the thumbnail image: "
+                                    + error.getMessage());
+                        }
+
+                        @Override
+                        public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                            // Success! there was an image url retrieved. Set the image in the imageview
+                            theImageView.setImageBitmap(response.getBitmap());
+                        }
+                    });
+                } else {
+                    // the url was not there
+                    theImageView.setImageResource(R.mipmap.image_notavailable_icon);
+                }
             }
         });
 
